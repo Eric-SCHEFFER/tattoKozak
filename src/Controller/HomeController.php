628c7a4f6b08a6794e86184bca2dcae66e048e8e
eric <eric.scheffer.pro@gmail.com>
2020-09-04 15:14:26 +0200

    affichage slogan

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Slogan;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -16,7 +16,13 @@ class HomeController extends AbstractController
 
    public function homeLoad()
    {
+      // On recupere le slogan depuis le SloganRepository avec la methode find (voir les autres: findAll, findOneBy, etc...)
+      $slogan = $this->getDoctrine()->getRepository(slogan::class)->find(1);
       
-      return $this->render('tatoo-kozak/pages/home.html.twig', ['menu_courant' => 'home']);
+      // On envoie à la vue les différentes variables, dont le slogan, dans un tableau 
+      return $this->render('tatoo-kozak/pages/home.html.twig', [
+         'menu_courant' => 'home',
+         'slogan' => $slogan,
+         ]);
    }
 }
