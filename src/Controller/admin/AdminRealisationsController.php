6eb909d574facd642c18b0b90e160912f07e7158
eric <eric.scheffer.pro@gmail.com>
2020-09-18 17:01:45 +0200

    back et front: continué aspace admin (controleur + twigs)

--- src/Controller/admin/AdminRealisationsController.php
+++ src/Controller/admin/AdminRealisationsController.php
@@ -1,19 +1,40 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RealisationsRepository;
 
 class AdminRealisationsController extends AbstractController
 {
+    private $repository;
+
+    public function __construct(RealisationsRepository $repository)
+    {
+
+
+        $this->repository = $repository;
+    }
+
     /**
-     * @Route("/admin/realisations", name="admin_realisations")
+     * @Route("/admin", name="admin.realisation")
      */
     public function index()
     {
-        return $this->render('admin_realisations/index.html.twig', [
-            'controller_name' => 'AdminRealisationsController',
+        $realisations = $this->repository->findAll();
+
+        return $this->render('admin/realisations/adminRealisations.html.twig', [
+            'realisations' => $realisations
         ]);
+    }
+
+    /**
+     * @Route("/admin/{id}", name="admin.realisation.edit")
+     */
+    public function edit()
+    {
+
+
     }
 }
