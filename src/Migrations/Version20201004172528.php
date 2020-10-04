843a08c1ddf1df38f61cc19c6682eedb65466a9a
eric <eric.scheffer.pro>
2020-10-04 19:41:51 +0200

    Clone de github et reglages

--- src/Migrations/Version20201004172528.php
+++ src/Migrations/Version20201004172528.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200923105835 extends AbstractMigration
+final class Version20201004172528 extends AbstractMigration
 {
     public function getDescription() : string
     {
