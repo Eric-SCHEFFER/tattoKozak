79ce53d7545dc2fedae768123bbd64f0433a7c0c
eric <eric.scheffer.pro@gmail.com>
2020-10-13 15:51:21 +0200

    feat: uploads multiples - Commencé Ajax traitement clic suppression image

--- public/assets/js/images.js
+++ public/assets/js/images.js
@@ -1,5 +1,34 @@
 window.onload = () => {
    // Gestion des liens "Supprimer"
    let links = document.querySelectorAll("[data-delete]")
-   console.log(links)
+
+   // On boucle sur links
+   for (link of links) {
+      // On écoute le clic
+      link.addEventListener("click", function (e) {
+         // On désactive le lien
+         e.preventDefault()
+         // On demande confirmation
+         if (confirm("Voulez-vous supprimer cette image ?")) {
+            // On envoi envoie une requette Ajax vers le href du lien avec la méthode DELETE
+            fetch(this.getAttribute("href"), {
+               method: "DELETE",
+               headers: {
+                  "X-Requsted-With": "XMLHttpRequest",
+                  "Content-Type": "application/json"
+               },
+               body: JSON.stringify({ "_token": this.dataset._token })
+            }).then(
+               // On récupère la réponse en JSON
+               response => response.json()
+            ).then(data => {
+               if (data.success)
+                  this.parentElement.remove
+               else
+                  alert(data.error)
+            }).catch(e => alert(e))
+         }
+
+      })
+   }
 }
