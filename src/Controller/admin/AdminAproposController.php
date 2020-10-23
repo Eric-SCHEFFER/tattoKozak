ff35e9995e9bf35e12279ac7e213e87f33e65b40
eric <eric.scheffer.pro@gmail.com>
2020-10-23 16:50:22 +0200

    feat: terminé form administration A propos

--- src/Controller/admin/AdminAproposController.php
+++ src/Controller/admin/AdminAproposController.php
@@ -7,18 +7,14 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\AProposEtInfos;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\AProposEtInfosRepository;
 use App\Form\AProposType;
 
 class AdminAproposController extends AbstractController
 {
-    public function __construct(AProposEtInfosRepository $aProposEtInfosRepository, EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em)
     {
-
         $this->em = $em;
     }
-
-
 
     /**
      * @Route("/admin/apropos", name="admin_apropos")
@@ -26,16 +22,16 @@ class AdminAproposController extends AbstractController
     // ======== ÉDITER A PROPOS ========
     public function edit(Request $request)
     {
-        // $aProposEtInfos = new AProposEtInfos();
-        $aProposEtInfos = $this->getDoctrine()->getRepository(AProposEtInfosRepository::class)->findAll();
-
+        // On récupère la table apropos_et_infos
+        $aProposEtInfos = $this->getDoctrine()->getRepository(AProposEtInfos::class)->findAll();
+        // Comme il n'y a qu'une seule ligne, ce sera l'index 0 du tableau
+        $aProposEtInfos = $aProposEtInfos[0];
         $form = $this->createForm(AProposType::class, $aProposEtInfos);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-
             $this->em->persist($aProposEtInfos);
             $this->em->flush();
-            $this->addFlash('succes', 'Succès de la mise à jour');
+            $this->addFlash('succes', 'Mis à jour avec succès');
             return $this->redirectToRoute('admin');
         }
         return $this->render('admin_apropos/index.html.twig', [
